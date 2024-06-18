cfc831f08101df26593b0e399de175a355015550
Abdullah-Ajmal147 <abajmal211@gmail.com>
2024-06-18 17:14:36 +0500

    allowed all the user

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+%df@wc)xoryscpd43%ar(%@h%pm=@g7v!tst2uwjfec9ydb$v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
