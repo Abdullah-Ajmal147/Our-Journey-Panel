78a4c86bc262196370b2393083a60c861fef5e36
Abdullah-Ajmal147 <abajmal211@gmail.com>
2024-06-21 01:26:21 +0500

    updat the statuc

--- core/urls.py
+++ core/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/auth/', include('authentication.urls.v1.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
