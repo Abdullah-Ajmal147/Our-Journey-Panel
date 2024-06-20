f41c6abe02bed4decba3047f3ffd811bbf2e1135
Abdullah-Ajmal147 <abajmal211@gmail.com>
2024-06-21 00:52:13 +0500

    update the statics

--- userData.sh
+++ userData.sh
@@ -2,18 +2,18 @@
 set -e
 
 # Replace {YOUR_GIT_REOPO_URL} with your actual Git repository URL
-GIT_REPO_URL="https://github.com/Abdullah-Ajmal147/Our-Journey-Panel.git"
+#GIT_REPO_URL="https://github.com/Abdullah-Ajmal147/Our-Journey-Panel.git"
 
 # If using Private Repo
 #GIT_REPO_URL="https://<your_username>:<your_PAT>@github.com/codewithmuh/django-aws-ec2-autoscaling.git"
 
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
-PROJECT_MAIN_DIR_NAME="Our-Journey-Panel"
+#PROJECT_MAIN_DIR_NAME="Our-Journey-Panel"
 
 # Clone repository
-git clone "$GIT_REPO_URL" "/home/ubuntu/backend/$PROJECT_MAIN_DIR_NAME"
+#git clone "$GIT_REPO_URL" "/home/ubuntu/backend/$PROJECT_MAIN_DIR_NAME"
 
-cd "/home/ubuntu/backend/$PROJECT_MAIN_DIR_NAME"
+#cd "/home/ubuntu/backend/$PROJECT_MAIN_DIR_NAME"
 
 # Make all .sh files executable
 chmod +x scripts/*.sh
