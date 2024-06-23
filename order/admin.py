00b3d9aca889b6032b3dd2b708c6c005075ef1a7
Abdullah-Ajmal147 <abajmal211@gmail.com>
2024-06-23 17:48:10 +0500

    added the admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Orders, Location
 
-# Register your models here.
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('address', 'latitude', 'longitude')
+    search_fields = ('address',)
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('user', 'from_location', 'to_location', 'ride_type', 'fare', 'payment_method', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'from_location__address', 'to_location__address')
+    list_filter = ('ride_type', 'payment_method', 'created_at', 'updated_at')
+    autocomplete_fields = ('user', 'from_location', 'to_location')
+
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Orders, OrdersAdmin)
--- order/urls/v1/urls.py
+++ order/urls/v1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from order.views.v1.views import OrderAPIView
 
 urlpatterns = [
-    path('order/', OrderAPIView.as_view(), name='order-list-create'),
+    path('', OrderAPIView.as_view(), name='order-list-create'),
 ]
