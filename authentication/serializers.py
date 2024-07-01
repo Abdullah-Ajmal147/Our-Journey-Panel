d403b8380bf2d5ba3c1d7055377f71311913e11c
Abdullah-Ajmal147 <abajmal211@gmail.com>
2024-07-01 11:07:31 +0500

    added the user id

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -5,7 +5,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['name', 'email', 'phone', 'country_code', 'role', 'ride_category']
+        fields = ['id','name', 'email', 'phone', 'country_code', 'role', 'ride_category']
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
