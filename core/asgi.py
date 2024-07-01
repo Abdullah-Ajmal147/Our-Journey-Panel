53d84fe2737a5d21b84fbdfaf74f72c11adab9e2
Abdullah-Ajmal147 <abajmal211@gmail.com>
2024-07-01 21:23:09 +0500

    update the routing

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,22 @@
 import os
 import django
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 import web_socket.routing
 
+# Ensure DJANGO_SETTINGS_MODULE is set
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Initialize Django application
 django.setup()
 
-# Your ASGI application definition
+# Import the ASGI application from your Django project's routing module
+# from core.routing import application as django_application
+
+# Combine Django ASGI application with WebSocket routing
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": get_asgi_application(),  # Django HTTP ASGI application
     "websocket": AuthMiddlewareStack(
         URLRouter(
             web_socket.routing.websocket_urlpatterns
