bd878574f7c5ded84c32753c1e1faa6bf4f02894
ehsan.sher <ehsan.sher@purelogics.net>
2024-07-12 23:40:39 +0500

    update view in chat

--- web_socket/urls.py
+++ web_socket/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('room/<str:room_name>/', room, name='room'),
-    path('send_message/', SendMessage.as_view(), name='send-message'),
+    path('message/', Message.as_view(), name='message'),
 
     path('caption_dashboard/', caption_dashboard, name='caption_dashboard'),
     path('user_dashboard/<str:room_name>/', user_dashboard, name='user_dashboard'),
